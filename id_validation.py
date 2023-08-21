8936f9f091099c4266b55d9b7510e78785e4c4af
U-rbot <96120978+U-rbot@users.noreply.github.com>
2023-08-21 16:53:15 +0530

    Update id_validation.py

--- id_validation.py
+++ id_validation.py
@@ -1,9 +1,3 @@
-# Read data from CSV file
-input_file = 'input_data.csv'
-df = pd.read_csv(input_file)
-# Create a DataFrame from the data
-
-
 import pandas as pd
 import re
 
@@ -23,32 +17,47 @@ def extract_5_digit_numbers(text):
     numbers = re.findall(r'\b\d{5}\b', text)
     return numbers
 
-# Find the unique IDs with 5-digit numbers in comments or uc id
+# Function to check if a list has at least one 5-digit number
+def has_5_digit_number(numbers_list):
+    for number in numbers_list:
+        if len(number) == 5 and number != '899583':
+            return True
+    return False
+
+# Find unique IDs with 5-digit numbers in comments or uc id
 unique_ids = df['id'].unique()
 result_data = []
 
 for id_ in unique_ids:
+    # Extract 5-digit numbers from comments and uc id columns for this ID
     comments = df[df['id'] == id_]['comments'].apply(extract_5_digit_numbers)
     uc_ids = df[df['id'] == id_]['uc id'].apply(extract_5_digit_numbers)
     
     found_status = 'Not found'
     uc_result = ''
     
+    # Check if any of the comments have a 5-digit number
     for comment_numbers in comments:
         if has_5_digit_number(comment_numbers):
             found_status = 'Found'
-            uc_result = comment_numbers[0]
+            uc_result = comment_numbers[0]  # Store the 5-digit number
             break
-    
+        
+    # If not found in comments, check uc id
     if found_status == 'Not found':
         for uc_numbers in uc_ids:
             if has_5_digit_number(uc_numbers):
                 found_status = 'Found'
-                uc_result = uc_numbers[0]
+                uc_result = uc_numbers[0]  # Store the 5-digit number
                 break
     
     result_data.append({'id': id_, 'uc': uc_result, 'Result': found_status})
 
 result_df = pd.DataFrame(result_data)
 
+# Write the result to a CSV file
+result_df.to_csv('result.csv', index=False)
+
+# Print the result DataFrame
 print(result_df)
+print("done")
